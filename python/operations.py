8d338ba5df9517d36b404c7684cfa68cdca1562e
K <koftyan@gmail.com>
2020-07-13 00:05:14 +0300

    add vector operations

---
+++ python/operations.py
@@ -0,0 +1,27 @@
+def elementwise_multiplication(vec_a, vec_b):
+  assert(len(vec_a) == len(vec_b))
+  product = []
+  for i in range(len(vec_a)):
+    product.append(vec_a[i] * vec_b[i])
+  return product
+
+def elementwise_addition(vec_a, vec_b):
+  assert(len(vec_a) == len(vec_b))
+  product = []
+  for i in range(len(vec_a)):
+    product.append(vec_a[i] + vec_b[i])
+  return product
+
+def vector_sum(vec):
+  sum = 0
+  for i in range(len(vec)):
+    sum += vec[i]
+  return sum
+
+def vector_average(vec):
+  return vector_sum(vec) / len(vec)
+
+print(elementwise_multiplication([1,2,3], [1,2,3]))
+print(elementwise_addition([1,2,3], [1,2,3]))
+print(vector_sum([1,2,3]))
+print(vector_average([1,2,3]))
